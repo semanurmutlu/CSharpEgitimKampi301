e155e1fa923dda81683f1841259e311c6c6f6cc4
semanurmutlu <semanurmutlu29@gmail.com>
2024-12-29 18:08:48 +0300

    ...

--- CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,14 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public int ProductStok { get; set; }
         public decimal ProductPrice { get; set; }
         public string ProductDescription { get; set; }
-        
+
         //-------İlişkilerimizi Kuruyoruz
         public int CategoryId { get; set; }//her ürünün 1 kategorisi olmalı
         public virtual Category Category { get; set; } //1 ürünün kategori bilgisine ulaşmam gerekiyor - kategori nin tüm bilgilerine erişmem içinde category türünde category propertysi oluşturmalıyım
         public List<Order> Orders { get; set; } //(ürün tablosu listeye dönecek) 1-m ilişki için her bir ürün siparişler olarak listelenebilmeli
+
+        //[NotMapped]
+        //public string CategoryName { get; set; }
 
     }
 }
---
+++ CSharpEgitimKampi301.PresentationLayer/FrmProduct.cs
@@ -0,0 +1,90 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.BusinessLayer.Concrete;
+using CSharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.PresentationLayer
+{
+    public partial class FrmProduct : Form
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        public FrmProduct()
+        {
+            InitializeComponent();
+            _productService = new ProductManager(new EfProductDal());
+            _categoryService = new CategoryManager(new EfCategoryDal());
+        }
+        // ProductManager productManager = new ProductManager(new EfProductDal());
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetAll();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnList2_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetProductsWithCategory();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            _productService.TDelete(value);
+            MessageBox.Show("Silme işlemi başarılı");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            product.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            product.ProductPrice = decimal.Parse(txtProductPrice.Text);
+            product.ProductName = txtProductName.Text;
+            product.ProductDescription = txtDescription.Text;
+            product.ProductStok = int.Parse(txtProductStock.Text);
+
+            _productService.TInsert(product);
+            MessageBox.Show("Eklme işlemi yapıldı.");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value =_productService.TGetById(id);
+            dataGridView1.DataSource = value;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            value.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            value.ProductDescription=txtDescription.Text;
+            value.ProductPrice=decimal.Parse(txtProductPrice.Text);
+            value.ProductStok = int.Parse(txtProductStock.Text);
+            value.ProductName=txtProductName.Text;
+            _productService.TUpdate(value);
+            MessageBox.Show("Güncelleme işlemi başarıyla yapıldı");
+        }
+
+        private void FrmProduct_Load(object sender, EventArgs e)
+        {
+            var values = _categoryService.TGetAll();
+            cmbCategory.DataSource = values;
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryId";
+        }
+    }
+}
